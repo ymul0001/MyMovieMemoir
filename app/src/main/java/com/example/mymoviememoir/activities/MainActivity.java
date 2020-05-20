7ec9803524970b14704f8699fad37f1b6feadb0d
Yonas Mulyadi <ymul0001@student.monash.edu>
2020-05-20 19:43:31 +0700

    finish the maps fragment

--- app/src/main/java/com/example/mymoviememoir/activities/MainActivity.java
+++ app/src/main/java/com/example/mymoviememoir/activities/MainActivity.java
@@ -92,7 +92,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         editor.putString("firstName", bundle.getString("firstName"));
         editor.putInt("personId", bundle.getInt("personId"));
         editor.apply();
-
     }
 
     @Override
--- app/src/main/java/com/example/mymoviememoir/fragments/MapsFragment.java
+++ app/src/main/java/com/example/mymoviememoir/fragments/MapsFragment.java
@@ -1,29 +1,25 @@
 package com.example.mymoviememoir.fragments;
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
+import android.util.Log; // BARU PERSON, BELUM CINEMANYA
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import com.example.mymoviememoir.R;
 import com.example.mymoviememoir.networkconnection.NetworkConnection;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -32,14 +28,21 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class MapsFragment extends Fragment implements OnMapReadyCallback {
 
     private GoogleMap gMaps;
     private MapView mapView;
-    private double longitude;
-    private double latitude;
+    private double userLongitude;
+    private double userLatitude;
+    private ArrayList<Double> cinemaLatitude = new ArrayList<>();
+    private ArrayList<Double> cinemaLongitude = new ArrayList<>();
+    private ArrayList<String> cinemaNames;
+    private MarkerOptions mo = new MarkerOptions();
     NetworkConnection networkConnection = null;
 
     public MapsFragment(){
@@ -51,8 +54,11 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.maps_fragment, container, false);
         networkConnection = new NetworkConnection();
+        cinemaNames = new ArrayList<>();
+        GetCinemaLocationTask getCinemaLocationTask = new GetCinemaLocationTask();
         GetPersonLocationTask getPersonLocationTask = new GetPersonLocationTask();
         getPersonLocationTask.execute();
+        getCinemaLocationTask.execute();
         return view;
     }
 
@@ -60,7 +66,6 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mapView = view.findViewById(R.id.app_map);
-
         if (mapView != null){
             mapView.onCreate(null);
             mapView.onResume();
@@ -70,13 +75,7 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        MapsInitializer.initialize(getContext());
-        LatLng userLocation = new LatLng(latitude, longitude);
         gMaps = googleMap;
-        gMaps.addMarker(new MarkerOptions().position(userLocation).title("My location"));
-        float zoomLevel = (float) 10.0;
-        gMaps.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,
-                zoomLevel));
     }
 
     public LatLng getLocationFromAddress(String strAddress) {
@@ -85,44 +84,95 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
         LatLng p1 = null;
         try {
             address = coder.getFromLocationName(strAddress, 5);
-            Log.d("String", String.valueOf(address.get(1).getLatitude()) + String.valueOf(address.get(1).getLongitude()));
             if (address == null) {
                 return null;
             }
             Address location = address.get(0);
-            p1 = new LatLng(location.getLatitude() * 1E6, location.getLongitude()* 1E6);
+            p1 =  new LatLng(location.getLatitude(), location.getLongitude());
         } catch (IOException ex) {
             ex.printStackTrace();
         }
         return p1;
     }
 
+
     private class GetPersonLocationTask extends AsyncTask<Void, Void, String> {
         @Override
         protected String doInBackground(Void... params) {
             JSONObject personObj = null;
             String address = "";
-            SharedPreferences sf = getContext().getSharedPreferences("dashboardPreferences", Context.MODE_PRIVATE);
+            String state = "";
+            SharedPreferences sf = getContext().getSharedPreferences("dashboardPreferences", MODE_PRIVATE);
             int personId = sf.getInt("personId", 0);
-            Log.d("string", "we have " + personId);
             String personResult = networkConnection.getPersonById(personId);
-            Log.d("string", personResult);
             try {
                 personObj = new JSONObject(personResult);
                 address = personObj.getString("personAddress");
+                state = personObj.getString("personState");
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-            LatLng coordinates = getLocationFromAddress(address);
-            String latitude  = String.valueOf(coordinates.latitude);
-            String longitude = String.valueOf(coordinates.longitude);
-            return latitude + "," + longitude;
+            LatLng coordinates = getLocationFromAddress(address + state);
+
+            userLatitude = coordinates.latitude;
+            userLongitude = coordinates.longitude;
+            return "person location has been retrieved";
         }
 
         @Override
-        protected void onPostExecute(String coordinates) {
-            latitude = Double.parseDouble(coordinates.split(",")[0]);
-            longitude = Double.parseDouble(coordinates.split(",")[1]);
+        protected void onPostExecute(String message) {
+            LatLng userLocation = new LatLng(userLatitude, userLongitude);
+            gMaps.addMarker(mo.position(userLocation).title("My location"));
+            float zoomLevel = (float) 15.0;
+            gMaps.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,
+                    zoomLevel));
+            Toast.makeText(getContext(), message,
+                    Toast.LENGTH_LONG).show();
         }
     }
+
+    private class GetCinemaLocationTask extends AsyncTask<Void, Void, String>{
+        @Override
+        protected String doInBackground(Void...params){
+            String result = networkConnection.getAllCinemas();
+            Log.d("string",result);
+            JSONArray cinemaArray = null;
+            try {
+                cinemaArray = new JSONArray(result);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            for(int i = 0; i < cinemaArray.length(); i++){
+                JSONObject cinemaObj = null;
+                try {
+                    cinemaObj = cinemaArray.getJSONObject(i);
+                    String fullAddress = cinemaObj.getString("cinemaAddress");
+                    LatLng coordinates = getLocationFromAddress(fullAddress);
+                    cinemaLatitude.add(coordinates.latitude);
+                    cinemaLongitude.add(coordinates.longitude);
+                    cinemaNames.add(cinemaObj.getString("cinemaName"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            return "All cinema location have been retrieved";
+        }
+
+        @Override
+        protected void onPostExecute(String message){
+            Toast.makeText(getContext(), message,
+                    Toast.LENGTH_LONG).show();
+            ArrayList<LatLng> cinemaCoordinates = new ArrayList<>();
+            for(int i = 0; i < cinemaLatitude.size(); i++)
+            {
+                cinemaCoordinates.add(new LatLng(cinemaLatitude.get(i), cinemaLongitude.get(i)));
+            };
+            for(LatLng coor: cinemaCoordinates){
+                mo.position(coor).title(cinemaNames.get(cinemaCoordinates.indexOf(coor))).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+                gMaps.addMarker(mo);
+            }
+        }
+    }
+
 }
