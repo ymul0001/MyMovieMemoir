c50b75ce97660ce797215f955c67f6fdb70b8cf2
Yonas Mulyadi <ymul0001@student.monash.edu>
2020-05-22 00:56:10 +0700

    add the activity for movie details

--- app/src/main/java/com/example/mymoviememoir/fragments/ReportsFragment.java
+++ app/src/main/java/com/example/mymoviememoir/fragments/ReportsFragment.java
@@ -4,7 +4,6 @@ import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Color;
-import android.net.Network;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
